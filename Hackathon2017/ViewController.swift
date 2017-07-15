a4bea45a7e9869162773463fe1aa59ba2ead1482
Kuba Janicki <kajotek1@gmail.com>
2017-07-15 19:12:49 +0200

    Init commit

---
+++ Hackathon2017/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hackathon2017
+//
+//  Created by Jakub Janicki on 15.07.2017.
+//  Copyright © 2017 CzorneSalami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
