d802ebab53acc25db87ec8f692d781575b96a152
Kuba Janicki <kajotek1@gmail.com>
2017-07-15 22:56:20 +0200

    Add ProfileNameLastnameTableViewCell

---
+++ iOSApp/Hackathon2017/ProfileNameLastnameTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileNameLastnameTableViewCell.swift
+//  Hackathon2017
+//
+//  Created by Jakub Janicki on 15.07.2017.
+//  Copyright © 2017 CzorneSalami. All rights reserved.
+//
+
+import UIKit
+
+class ProfileNameLastnameTableViewCell: UITableViewCell {
+
+    class func reuseIdentifier() -> String {
+        return "ProfileNameLastnameTableViewCell"
+    }
+    
+}
