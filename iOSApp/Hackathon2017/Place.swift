472cf3d49afda82adc7ee00d0add865d723aea48
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2017-07-16 01:18:57 +0200

    Add place and route models

---
+++ iOSApp/Hackathon2017/Place.swift
@@ -0,0 +1,15 @@
+//
+//  Place.swift
+//  Hackathon2017
+//
+//  Created by Arkadiusz Żmudzin on 16.07.2017.
+//  Copyright © 2017 CzorneSalami. All rights reserved.
+//
+
+import Foundation
+
+class Place {
+    var name: String?
+    var latitude: Double?
+    var longitude: Double?
+}
---
+++ iOSApp/Hackathon2017/Route.swift
@@ -0,0 +1,14 @@
+//
+//  Route.swift
+//  Hackathon2017
+//
+//  Created by Arkadiusz Żmudzin on 16.07.2017.
+//  Copyright © 2017 CzorneSalami. All rights reserved.
+//
+
+import Foundation
+
+class Route {
+    var encodedPath: String?
+    var waypoints: [Place]?
+}
