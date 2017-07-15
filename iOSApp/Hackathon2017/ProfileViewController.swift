78803f7e5b87aac18dd9b6d5f69cf6aa625b5c3b
Kuba Janicki <kajotek1@gmail.com>
2017-07-15 22:55:20 +0200

    Add UITableView to ProfileViewController

--- iOSApp/Hackathon2017/ProfileViewController.swift
+++ iOSApp/Hackathon2017/ProfileViewController.swift
@@ -8,28 +8,61 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var imageViewDim: CGFloat!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.setupTableView()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func setupTableView() {
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        self.tableView.register(UINib(nibName: "ProfileAvatarTableViewCell", bundle: nil), forCellReuseIdentifier: ProfileAvatarTableViewCell.reuseIdentifier())
+        self.tableView.register(UINib(nibName: "ProfileNameLastnameTableViewCell", bundle: nil), forCellReuseIdentifier: ProfileNameLastnameTableViewCell.reuseIdentifier())
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.row == 0 {
+            return self.imageViewDim + 40
+        } else if indexPath.row == 1 {
+            return 105
+        } else {
+            return 40
+        }
     }
-    */
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        if indexPath.row == 0 {
+            return self.setupAvatarCell(tableView: tableView, indexPath: indexPath, imageViewDim: self.imageViewDim)
+        } else if indexPath.row == 1 {
+            return self.setupNameLastnameCell(tableView: tableView, indexPath: indexPath)
+        } else {
+            return UITableViewCell()
+        }
+    }
+    
+    func setupAvatarCell(tableView: UITableView, indexPath: IndexPath, imageViewDim: CGFloat) -> ProfileAvatarTableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ProfileAvatarTableViewCell.reuseIdentifier(), for: indexPath) as! ProfileAvatarTableViewCell
+        cell.setupCell(imageDim: imageViewDim)
+        return cell
+    }
+    
+    func setupNameLastnameCell(tableView: UITableView, indexPath: IndexPath) -> ProfileNameLastnameTableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ProfileNameLastnameTableViewCell.reuseIdentifier(), for: indexPath) as! ProfileNameLastnameTableViewCell
+        return cell
+    }
+    
+    
 
 }
