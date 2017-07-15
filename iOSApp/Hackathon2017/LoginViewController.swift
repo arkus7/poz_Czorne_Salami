b4b5801c65938f8597a5764869355a6e0e385803
Kuba Janicki <kajotek1@gmail.com>
2017-07-15 20:49:56 +0200

    Update project structure

--- iOSApp/Hackathon2017/LoginViewController.swift
+++ iOSApp/Hackathon2017/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  Hackathon2017
 //
 //  Created by Jakub Janicki on 15.07.2017.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
