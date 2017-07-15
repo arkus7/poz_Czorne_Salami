5b3d51260552c20dd14621d6f082bd9e27b7f89e
Kuba Janicki <kajotek1@gmail.com>
2017-07-15 21:25:45 +0200

    Add Login layout

---
+++ iOSApp/Hackathon2017/RoundedImage.swift
@@ -0,0 +1,20 @@
+//
+//  RoundedImage.swift
+//  Hackathon2017
+//
+//  Created by Jakub Janicki on 15.07.2017.
+//  Copyright © 2017 CzorneSalami. All rights reserved.
+//
+
+import UIKit
+
+class RoundedImage: UIImageView {
+    
+    override func layoutSubviews() {
+        self.layer.borderWidth = 1
+        self.layer.masksToBounds = false
+        self.layer.borderColor = UIColor.black.cgColor
+        self.layer.cornerRadius = self.frame.height / 2
+        self.clipsToBounds = true
+    }
+}
