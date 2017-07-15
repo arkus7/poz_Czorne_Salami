ad0b2da61c4de70578ee1658f1114fece51851d2
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2017-07-16 01:47:59 +0200

    Update map view controller

--- iOSApp/Hackathon2017/MapViewController.swift
+++ iOSApp/Hackathon2017/MapViewController.swift
@@ -22,13 +22,13 @@ class MapViewController: UIViewController {
         self.route = Route()
         route.encodedPath = "www~Hgn{eBqFaKu@iA}FkHsBsCYq@a@uAoC`CwAlA]RX~AlAtGLbAMcAmAuGY_B\\SvAmA|BqBPOSaAsBuLiBoKkAyGgCsOqDuU{AwKi@{ByB{MmB{KkJeg@K}@|@]bBm@tEWp@CRDB_@NsBp@{G|C}[xCbBnGzDPL~ChBz@j@{@k@_DiBIGb@iCb@gCbBsKuEuE{EmE}AiBSQKC}@R{E|@yFnAaBV_ALuC@qAAoASmHoA{@QG[oAqIgAoI[mDUeFUqLCqA@{@\\gFRqCDoD\\}AF_@gCiAoBq@KKWWYKY@SF_AK}EgAuCi@}@[qBa@sAWk@Se@OaAe@o@_@aAu@kBkBK_@Ec@c@_A[QcA_@SIGKyAcBiAeBy@}As@aBs@cBIYk@y@qAsAeAcA{C{C{BwBwD_EcE}DWM_@IyAEc@BiCnJkA~De@lAo@|@i@f@o@\\o@P[Dq@@oDc@iAY_F{A}AOiGEgC?sNb@{ADsA?_AIcB_@yBo@mBe@@YgA[yAc@IAQBWIMGiBQOGOKK?m@Ng@LyC~@aGRuCJc@@ASCIaG?gE@YDgCXw@h@}ABoB@iCCwEQgDAuEH_DF[EcBD{CJq@WoCKO}FUU?eDCwFEgS}BMiK[wA?"
         let place1 = Place()
-        place1.latitude = 52.3917953
-        place1.longitude = 16.857482
-        place1.name = "Pixel, Grunwaldzka 182, 60-166 Poznań, Polska"
+        place1.coordinates = [52.3917953, 16.857482]
+        place1.name = "Pixel"
+        place1.address = "Grunwaldzka 182, 60-166 Poznań, Polska"
         let place2 = Place()
-        place2.latitude = 52.4537902
-        place2.longitude = 16.9275968
-        place2.name = "Umultowska 45, Poznań, Polska"
+        place2.coordinates = [52.4537902, 16.9275968]
+        place2.name = "Dom"
+        place2.address = "Umultowska 45, Poznań, Polska"
         route.waypoints = [place1, place2]
         
         showPath(mapView)
@@ -49,6 +49,7 @@ class MapViewController: UIViewController {
         waypoints.forEach {
             let marker = GMSMarker(position: CLLocationCoordinate2D(latitude: $0.latitude!, longitude: $0.longitude!))
             marker.title = $0.name
+            marker.snippet = $0.address
             marker.map = mapView
         }
         
