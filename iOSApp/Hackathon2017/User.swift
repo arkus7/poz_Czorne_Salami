0282b4d0f00ecdfb72cbb72786c0dac5cdae0c20
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2017-07-16 05:20:03 +0200

    Add User model

---
+++ iOSApp/Hackathon2017/User.swift
@@ -0,0 +1,52 @@
+//
+//  User.swift
+//  Hackathon2017
+//
+//  Created by Arkadiusz Żmudzin on 16.07.2017.
+//  Copyright © 2017 CzorneSalami. All rights reserved.
+//
+
+import Foundation
+import ObjectMapper
+
+class TimeTable: Mappable {
+    var monday: [Int]?
+    var tuesday: [Int]?
+    var wednesday: [Int]?
+    var thursday: [Int]?
+    var friday: [Int]?
+    var saturday: [Int]?
+    var sunday: [Int]?
+    
+    required init?(map: Map) {
+    
+    }
+    
+    func mapping(map: Map) {
+        monday <- map["0"]
+        tuesday <- map["1"]
+        wednesday <- map["2"]
+        thursday <- map["3"]
+        friday <- map["4"]
+        saturday <- map["5"]
+        sunday <- map["6"]
+    }
+}
+
+class User : Mappable {
+    var name: String?
+    var surname: String?
+    var timetable: TimeTable?
+    var placesIds: [Int]?
+    
+    required init?(map: Map) {
+        
+    }
+    
+    func mapping(map: Map) {
+        name <- map["name"]
+        surname <- map["surname"]
+        timetable <- map["timetable"]
+        placesIds <- map["places"]
+    }
+}
