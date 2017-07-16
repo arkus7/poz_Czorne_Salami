23728293a1737406f7ef799915d4042083f1f71d
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2017-07-16 03:41:37 +0200

    Update Place model

--- iOSApp/Hackathon2017/Place.swift
+++ iOSApp/Hackathon2017/Place.swift
@@ -7,8 +7,9 @@
 //
 
 import Foundation
+import ObjectMapper
 
-class Place {
+class Place : Mappable {
     var name: String?
     var coordinates: [Double]?
     var category: String?
@@ -17,11 +18,25 @@ class Place {
     var imageUrl: String?
     var placeDescription: String?
     
+    required init?(map: Map) {
+        
+    }
+    
+    func mapping(map: Map) {
+        self.name <- map["name"]
+        self.coordinates <- map["coordinates"]
+        self.category <- map["category"]
+        self.address <- map["address"]
+        self.city <- map["city"]
+        self.imageUrl <- map["imageUrl"]
+        self.placeDescription <- map["description"]
+    }
+    
     var latitude: Double? {
-        return coordinates?.first
+        return coordinates?.last
     }
     
     var longitude: Double? {
-        return coordinates?.last
+        return coordinates?.first
     }
 }
